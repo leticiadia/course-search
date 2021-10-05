52166867984c9804dfc9a98386291827bc89fe50
Leticia <contatoleticiadia@gmail.com>
2021-10-05 07:57:27 -0300

    feat: add require to import autoload

--- courseSearch.php
+++ courseSearch.php
@@ -1,10 +1,21 @@
 <?php
 
+    require 'vendor/autoload.php';
+
     use GuzzleHttp\Client;
+    use Symfony\Component\DomCrawler\Crawler;
 
     $client = new Client(['verify' => false]);
     $response = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
 
     $html = $response->getBody();
 
+    $crawler = new Crawler();
+    $crawler->addHTMLContent($html);
+
+    $cursos = $crawler->filter('span.card-curso__nome');
+
+    foreach($cursos as $curso){
+        echo $curso->textContent . PHP_EOL;
+    }
 ?>
